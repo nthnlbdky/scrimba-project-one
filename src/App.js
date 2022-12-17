177b196dbc375ea212a681760a4f03f4d0e18c5b
nathanbud <nthnlbdky916@gmail.com>
2022-12-17 09:51:23 +0800

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import {Main} from './components/Main';
 import Navbar  from './components/Navbar';
+import Footer  from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Navbar/>
       </header>
       <Main/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>NTHNLBDKY Road to React 2022</div>
+  )
+}
+
+export default Footer
